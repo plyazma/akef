72ad7549dc03dec3585d2f6697630cbcd029d145
plyazma <konzeliz21@gmail.com>
2026-02-10 15:47:30 +0200

    -

--- script.js
+++ script.js
@@ -11,4 +11,44 @@ sidebarButtons.forEach(sidebarButton => {
     });
 });
 
-//Tab 1
+// Tab 1
+
+function getNum(id) {
+    const isNum = document.getElementById(id);
+    return Number(isNum.value ?? isNum.textContent) || 0;
+}
+
+function updateStation(prefix) {
+    document.getElementById(`${prefix}-earning_estimates`).textContent =
+        getNum(`${prefix}-earning_efficiency`) * (100 + getNum(`${prefix}-bonus_earning_chance`)) / 100;
+}
+
+const regions = {
+    "valley4_ov": ["refugee_camp", "infra_station", "reconstruction_hq"],
+    "wuling_ov": ["sky_king_flats_construction_site"]
+}
+
+function updateRegion(regionId) {
+    regions[regionId].forEach(updateStation);
+    document.getElementById(`${regionId}-earning_estimates`).textContent =
+        regions[regionId].reduce((sum, p) => sum + getNum(`${p}-earning_estimates`), 0);
+}
+
+document.querySelectorAll("input").forEach(input => {
+    input.addEventListener("input", function() {
+        const prefix = this.id.split("-")[0];
+
+        for (const regionId in regions) {
+            if (regions[regionId].includes(prefix)) {
+                updateRegion(regionId);
+                break;
+            }
+        }
+    });
+});
+
+for (const regionId in regions) updateRegion(regionId)
+
+// Tab 2 // Valley IV
+
+function updateFactory
